f326d1ba523bd8266111cd2fb92625f6a16a6f98
wminhyuk <seoseo1998@gmail.com>
2025-02-10 17:53:26 +0900

    visualstart

---
+++ 5_Api.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="API", page_icon="🍽️")
+
+st.markdown("# 🍽️ API")
+st.sidebar.header("나이게산기")
+
+dt = st.date_input("생일입력")
+if st.button(
+
+
+https://ac.seo-sigma.shop/api/py/docs
+
